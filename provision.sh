a0148621c6c70afa857cefa06dd7a2e18e569d8d
Owain Lewis <owain@owainlewis.com>
2015-05-20 13:31:53 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,11 +1,24 @@
+#!usr/bin/env bash
+
+########################################
+
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y --force-yes ubuntu-desktop
 
+########################################
 # Install software essentials
+########################################
 
 sudo apt-get install -y \
 git \
 curl \
 zsh \
 gnome-terminal
+
+########################################
+## Docker
+########################################
+
+curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+sudo adduser vagrant docker
