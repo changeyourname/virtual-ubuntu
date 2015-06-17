f4f245034b9ec28acceac6eb637349aad7558365
Owain Lewis <owain@owainlewis.com>
2015-06-17 11:40:26 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -8,12 +8,12 @@ sudo apt-get upgrade
 ########################################
 
 sudo apt-get install -y --force-yes \
+build-essential \
 ubuntu-desktop \
 linux-headers-server \
 dkms \
 virtualbox-guest-x11 \
 virtualbox-guest-additions \
-build-essential \
 git \
 curl \
 zsh \
