27f69137af9ca96d779356da14d51e2fbb4ce616
Owain Lewis <owain@owainlewis.com>
2015-06-22 20:41:23 +0100

    working 15.04

--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
 ########################################
 # Provisioning Script for Ubuntu Dev Env
 ########################################
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
-sudo apt-get upgrade
 
 ########################################
 # Install software essentials
@@ -10,15 +10,14 @@ sudo apt-get upgrade
 sudo apt-get install -y --force-yes \
 build-essential \
 ubuntu-desktop \
-linux-headers-server \
-dkms \
-virtualbox-guest-x11 \
+virtualbox-guest-dkms \
 git \
 curl \
-zsh \
-guake
+zsh
 
 ########################################
 # ZSH
 ########################################
 sudo curl -L http://install.ohmyz.sh | sh
+
+echo 'Done'
